a32205deb030696204ba73c888f32402310925f0
shahvezawan <90408932+shahvezawan@users.noreply.github.com>
2026-07-09 18:48:38 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   // Replace <repository-name> with your exact GitHub repository name
   base: '/Smart-Pantry/', 
