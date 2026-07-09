bf511c9585454ce8f0dec05d7fee6be2c7742d4b
shahvezawan <90408932+shahvezawan@users.noreply.github.com>
2026-07-09 18:56:12 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Smart-Pantry/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
