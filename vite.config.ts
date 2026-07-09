03ce9cb8b2f2eef70790b404218847a2db4792f7
shahvezawan <90408932+shahvezawan@users.noreply.github.com>
2026-07-09 18:52:32 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
+export default defineConfig(() => {
+  return {
+    base: './',
     plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
   // Replace <repository-name> with your exact GitHub repository name
   base: '/Smart-Pantry/', 
 })
